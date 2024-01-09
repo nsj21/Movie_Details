aaa5cba6ca5128e512f8a30ec8a2638c6124f34c
Namrata Singh jadon <67650983+nsj21@users.noreply.github.com>
2024-01-09 19:04:07 +0530

    Add files via upload

---
+++ app1.js
@@ -0,0 +1,23 @@
+let api = 'https://www.omdbapi.com/?i=tt3896198&apikey=f23d97d6&t='
+
+
+
+
+function searchMovie(){
+    let query = document.getElementById('movieName').value;
+    let search = api+query;
+    fetch(search).then(function(data){
+        return data.json()
+    }).then(function(data){
+        document.getElementById('title').innerText = data.Title;
+        document.getElementById('desc').innerText = data.Plot;
+        document.getElementById('genre').innerText = data.Genre;
+        document.getElementById('actors').innerText = data.Actors;
+        document.getElementById('directors').innerText = data.Director;
+        document.getElementById('awards').innerText = data.Awards;
+        document.getElementById('collection').innerText = data.BoxOffice;
+        document.getElementById('ratings').innerText = data.imdbRating;
+        document.getElementById('writers').innerText = data.Writer;
+        document.getElementById('poster').src = data.Poster;
+    })
+}
